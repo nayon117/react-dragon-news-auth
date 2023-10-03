3d1b707f690769cf1d7a8b05470487c5c84bd2c0
nayon117 <hasibulhasannayon260@gmail.com>
2023-10-03 08:22:56 +0600

    add react-router tailwind and firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjCKsd9ha4so-bo4Y5uiEGTooJ6bJK4Bw",
+  authDomain: "react-dragon-news-auth-fb931.firebaseapp.com",
+  projectId: "react-dragon-news-auth-fb931",
+  storageBucket: "react-dragon-news-auth-fb931.appspot.com",
+  messagingSenderId: "536268704429",
+  appId: "1:536268704429:web:657ba8fdd16039fca05424"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
