b3cb57a6420beec6ac567121f7bf6018a257698b
nayon117 <hasibulhasannayon260@gmail.com>
2023-10-04 20:49:25 +0600

    some modifications

--- src/pages/Home/BreakingNews.jsx
+++ src/pages/Home/BreakingNews.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const BreakingNews = () => {
   return (
     <div className="flex gap-5 pt-10">
-      <button className="btn btn-secondary">Breaking News</button>
+      <button className="btn btn-sm btn-secondary">Breaking News</button>
       <Marquee pauseOnHover={true}>
       <Link to='/'>Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</Link>
       </Marquee>
